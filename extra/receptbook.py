3fdb9ccc9df036606206332bea7e20822df1b53a
nickdemeyer <nick.demeyer.programming@proton.me>
2026-08-28 19:48:36 +0200

    start project

---
+++ extra/receptbook.py
@@ -0,0 +1,12 @@
+recepts = []
+
+def view_recepts(recept):
+    
+while True:
+    print("=== RECEPT BOOK MENU ===")
+    print("1. View Recepts")
+    print("2. Add Recepts")
+    print("3. Search Recept")
+    print("4. Close App")
+
+    choice = input("Choose: ")
